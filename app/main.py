68b86c7694cb06d354a9d7e8701dcc54940493db
JillNandaha <jeellnandaha@gmail.com>
2026-07-02 12:25:08 +0300

    Added a frontend UI

--- app/main.py
+++ app/main.py
@@ -3,8 +3,16 @@ from fastapi import FastAPI, UploadFile, File
 from app.ingestion import extract_text, chunk_text
 from app.vectorstore import add_chunks,search_chunks
 from app.rag import generate_answer
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Document Intelligence API")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class QueryRequest(BaseModel):
     question: str
